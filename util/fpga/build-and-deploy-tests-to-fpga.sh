09a59050de7269182465bd341fe8ee8e3ffc69d5
Carl Lundin <clundin@google.com>
2025-08-13 20:03:39 -0700

    Fix cargo cache

--- util/fpga/build-and-deploy-tests-to-fpga.sh
+++ util/fpga/build-and-deploy-tests-to-fpga.sh
@@ -4,11 +4,13 @@ REPO_TOP="$(git rev-parse --show-toplevel)"
 
 # Cross compile the tests for the FPGA environment.
 docker run --rm -t \
-  -v cargo_cache:/usr/local/cargo/registry \
+  -v $HOME/.cargo/registry/cache:/root/.cargo/registry/cache \
+  -v $HOME/.cargo/registry/git:/root/.cargo/registry/git  \
   -v "${PWD}":/work-dir \
+  -w /work-dir \
   caliptra-fpga:latest \
   /bin/bash \
-  -c "(cd /work-dir && . util/fpga/cross-compiling/build-fpga-tests.sh)"
+  -c ". util/fpga/cross-compiling/build-fpga-tests.sh"
 
 # Copy the tests to the FPGA board.
 rsync -avzP \
