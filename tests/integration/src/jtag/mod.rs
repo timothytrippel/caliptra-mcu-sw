375f969fa3e6b10ae419f4e58f530a8ca1e31263
Tim Trippel <ttrippel@google.com>
2025-11-19 15:34:27 -0800

    [test] extend manuf debug unlock test to read DMI registers
    This extends the manuf debug unlock test to read the DMI registers that
    should all be accessible after a debug unlock operation.
    
    Signed-off-by: Tim Trippel <ttrippel@google.com>

--- tests/integration/src/jtag/mod.rs
+++ tests/integration/src/jtag/mod.rs
@@ -1,5 +1,7 @@
 // Licensed under the Apache-2.0 license
 
+#![allow(dead_code)]
+
 mod test_jtag_taps;
 mod test_lc_transitions;
 mod test_manuf_debug_unlock;
@@ -7,10 +9,68 @@ mod test_uds;
 
 #[cfg(test)]
 mod test {
+    use caliptra_hw_model::jtag::CaliptraCoreReg;
     use caliptra_hw_model::Fuses;
     use mcu_builder::FirmwareBinaries;
     use mcu_hw_model::{DefaultHwModel, InitParams, McuHwModel};
     use mcu_rom_common::LifecycleControllerState;
+
+    use std::sync::OnceLock;
+
+    static CALIPTRA_CORE_ALWAYS_UNLOCKED_DMI_REGS: OnceLock<Vec<CaliptraCoreReg>> = OnceLock::new();
+    static CALIPTRA_CORE_DEFAULT_LOCKED_DMI_REGS: OnceLock<Vec<CaliptraCoreReg>> = OnceLock::new();
+
+    pub fn get_cc_always_unlocked_dmi_regs() -> &'static Vec<CaliptraCoreReg> {
+        let mut s: Vec<CaliptraCoreReg> = Vec::new();
+        CALIPTRA_CORE_ALWAYS_UNLOCKED_DMI_REGS.get_or_init(|| {
+            s.push(CaliptraCoreReg::MboxDlen);
+            s.push(CaliptraCoreReg::MboxDout);
+            s.push(CaliptraCoreReg::MboxStatus);
+            s.push(CaliptraCoreReg::MboxCmd);
+            s.push(CaliptraCoreReg::MboxLock);
+            s.push(CaliptraCoreReg::MboxDin);
+            s.push(CaliptraCoreReg::MboxExecute);
+            s.push(CaliptraCoreReg::BootStatus);
+            s.push(CaliptraCoreReg::CptraHwErrrorEnc);
+            s.push(CaliptraCoreReg::CptraFwErrorEnc);
+            s.push(CaliptraCoreReg::BootfsmGo);
+            s.push(CaliptraCoreReg::CptraDbgManufServiceReg);
+            s.push(CaliptraCoreReg::HwFatalError);
+            s.push(CaliptraCoreReg::FwFatalError);
+            s.push(CaliptraCoreReg::HwNonFatalError);
+            s.push(CaliptraCoreReg::FwNonFatalError);
+            s.push(CaliptraCoreReg::SsDbgManufServiceRegReq);
+            s.push(CaliptraCoreReg::SsDbgManufServiceRegRsp);
+            s
+        })
+    }
+
+    pub fn get_cc_default_locked_dmi_regs() -> &'static Vec<CaliptraCoreReg> {
+        CALIPTRA_CORE_DEFAULT_LOCKED_DMI_REGS.get_or_init(|| {
+            let mut s: Vec<CaliptraCoreReg> = Vec::new();
+            s.push(CaliptraCoreReg::SsUdsSeedBaseAddrL);
+            s.push(CaliptraCoreReg::SsUdsSeedBaseAddrH);
+            s.push(CaliptraCoreReg::SsDebugIntent);
+            s.push(CaliptraCoreReg::SsCaliptraBaseAddrL);
+            s.push(CaliptraCoreReg::SsCaliptraBaseAddrH);
+            s.push(CaliptraCoreReg::SsMciBaseAddrL);
+            s.push(CaliptraCoreReg::SsMciBaseAddrH);
+            s.push(CaliptraCoreReg::SsRecoveryIfcBaseAddrL);
+            s.push(CaliptraCoreReg::SsRecoveryIfcBaseAddrH);
+            s.push(CaliptraCoreReg::SsExternalStagingAreaBaseAddrL);
+            s.push(CaliptraCoreReg::SsExternalStagingAreaBaseAddrH);
+            s.push(CaliptraCoreReg::SsOtpFcBaseAddrL);
+            s.push(CaliptraCoreReg::SsOtpFcBaseAddrH);
+            s.push(CaliptraCoreReg::SsStrapCaliptraDmaAxiUser);
+            s.push(CaliptraCoreReg::SsStrapGeneric0);
+            s.push(CaliptraCoreReg::SsStrapGeneric1);
+            s.push(CaliptraCoreReg::SsStrapGeneric2);
+            s.push(CaliptraCoreReg::SsStrapGeneric3);
+            s.push(CaliptraCoreReg::SsDbgUnlockLevel0);
+            s.push(CaliptraCoreReg::SsDbgUnlockLevel1);
+            s
+        })
+    }
 
     pub fn ss_setup(
         initial_lc_state: Option<LifecycleControllerState>,
--- tests/integration/src/jtag/test_manuf_debug_unlock.rs
+++ tests/integration/src/jtag/test_manuf_debug_unlock.rs
@@ -6,17 +6,33 @@ mod test {
     use std::thread;
     use std::time::Duration;
 
-    use crate::jtag::test::ss_setup;
+    use crate::jtag::test::{
+        get_cc_always_unlocked_dmi_regs, get_cc_default_locked_dmi_regs, ss_setup,
+    };
 
     use caliptra_api::mailbox::CommandId;
     use caliptra_hw_model::jtag::CaliptraCoreReg;
-    use caliptra_hw_model::openocd::openocd_jtag_tap::{JtagParams, JtagTap};
+    use caliptra_hw_model::openocd::openocd_jtag_tap::{JtagParams, JtagTap, OpenOcdJtagTap};
     use caliptra_hw_model::HwModel;
     use caliptra_hw_model::DEFAULT_MANUF_DEBUG_UNLOCK_RAW_TOKEN;
     use mcu_hw_model::jtag::jtag_send_caliptra_mailbox_cmd;
     use mcu_rom_common::LifecycleControllerState;
 
     use zerocopy::IntoBytes;
+
+    fn read_dmi_regs(
+        tap: &mut OpenOcdJtagTap,
+        regs: &Vec<CaliptraCoreReg>,
+    ) -> Vec<(CaliptraCoreReg, u32)> {
+        let mut reg_vals = Vec::new();
+        for reg in regs {
+            let val = tap
+                .read_reg(reg)
+                .expect("Failed to read DMI reg after unlock.");
+            reg_vals.push((*reg, val));
+        }
+        reg_vals
+    }
 
     #[test]
     fn test_manuf_debug_unlock() {
@@ -39,6 +55,15 @@ mod test {
             .jtag_tap_connect(&jtag_params, JtagTap::CaliptraCoreTap)
             .expect("Failed to connect to the Caliptra Core JTAG TAP.");
         println!("Connected.");
+
+        // Attempt to read all the ALWAYS unlocked DMI registers before unlock.
+        // We put the values in a Vec so we can print them in a contiguous
+        // block at the end of the test (without interleaved OpenOCD console
+        // messages).
+        let unlocked_reg_vals_before = read_dmi_regs(&mut *tap, get_cc_always_unlocked_dmi_regs());
+
+        // Attempt to read all the locked DMI registers before unlock.
+        let locked_reg_vals_before = read_dmi_regs(&mut *tap, get_cc_default_locked_dmi_regs());
 
         // Request manuf debug unlock operation.
         tap.write_reg(&CaliptraCoreReg::SsDbgManufServiceRegReq, 0x1)
@@ -73,6 +98,26 @@ mod test {
             }
             model.base.step();
             thread::sleep(Duration::from_millis(100));
+        }
+
+        // Attempt to read all the NEWLY unlocked DMI registers.
+        let locked_reg_vals_after = read_dmi_regs(&mut *tap, get_cc_default_locked_dmi_regs());
+
+        // Print the DMI reg values read throughout the test sequence.
+        println!("ALWAYS unlocked DMI regs (before) ...");
+        for (reg, val) in &unlocked_reg_vals_before {
+            print!("\tDMI reg: {:?}; ", reg);
+            println!("value = 0x{:08x?}", val);
+        }
+        println!("LOCKED DMI regs (before) ...");
+        for (reg, val) in &locked_reg_vals_before {
+            print!("\tDMI reg: {:?}; ", reg);
+            println!("value = 0x{:08x?}", val);
+        }
+        println!("LOCKED DMI regs (after) ...");
+        for (reg, val) in &locked_reg_vals_after {
+            print!("\tDMI reg: {:?}; ", reg);
+            println!("value = 0x{:08x?}", val);
         }
     }
 }
