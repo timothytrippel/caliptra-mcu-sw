4f339f555ec405a9c0ef1e5df3a27d7ef6d6a29a
Tim Trippel <ttrippel@google.com>
2026-01-30 14:49:49 -0800

    [rom] enable all DFT, HW debug, and prod debug levels
    The reference fuse map for Caliptra Subsystem has 8 prod debug unlock
    public key hash slots in the fuse map. This means there are 8 different
    debug unlock levels possible. This updates the MCU ROM to enable DFT, HW
    debug, and prod debug unlock across all debug unlock levels. This is
    primarily enabled for testing purposes. However, it is up to the
    integrator to customize their MCU ROM accordingly.
    
    Signed-off-by: Tim Trippel <ttrippel@google.com>

--- rom/src/rom.rs
+++ rom/src/rom.rs
@@ -435,6 +435,18 @@ impl Soc {
             );
             mci.registers.mci_reg_prod_debug_unlock_pk_hash_reg[i].set(word);
         }
+        // Set the debug enablement masks for: DFT, HW Debug, Prod Debug.
+        //
+        // Note: this enables all 8 debug levels (supported by the 8 prod debug
+        // unlock public key hash slots in the reference fuse map) to unlock
+        // DFT, HW debug, and prod debug access. Integrators should change this
+        // based on their integration.
+        mci.registers.mci_reg_soc_dft_en[0].set(0x000000FF);
+        mci.registers.mci_reg_soc_dft_en[1].set(0x00000000);
+        mci.registers.mci_reg_soc_hw_debug_en[0].set(0x000000FF);
+        mci.registers.mci_reg_soc_hw_debug_en[1].set(0x00000000);
+        mci.registers.mci_reg_soc_prod_debug_state[0].set(0x000000FF);
+        mci.registers.mci_reg_soc_prod_debug_state[1].set(0x00000000);
 
         // OCP LOCK Fuses.
         romtime::println!("[mcu-fuse-write] Attempting to write OCP LOCK fuses");
