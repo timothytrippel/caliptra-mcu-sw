29f7fa7ad40f0a0edadaed56c758a0bc5846f6cd
ternst-nv <ternst@nvidia.com>
2026-02-12 16:20:23 -0500

    firmware-bundler: Integration (#874)
    * firmware-bundler: Add manifests for emu + fpga (#705)
    
    Add manifests which build the emulator and fpga images.  This includes
    images with both the user and example application for both platforms.
    Remove the example manifest as well, since the various deployable
    manifests are more useful.
    
    * firmware-bundler: Add McuImageHeader to bundle (#705)
    
    Add the ability to generate an McuImagerHeader to the bundle command.
    This is triggered by adding an SVN field which is then serialized into
    the beginning of the output bundle.  As part of this adjust the
    allocation strategy and remove unnecessary validation steps.
    
    * firmware-bundler: Add rom vs. runtime features (#705)
    
    Support different arguments for rom vs. runtime features as they can
    differ between the deployment environments.
    
    * firmware-bundler: Improve directory utils (#705)
    
    Improve the directory utils to reduce code duplication.  These utils are
    also helpful for integrating the firmware-bundler into the existing build
    structure.
    
    * firmware-bundler: Account for grant in data alloc (#705)
    
    In dynamic sizing the output of the data section is used to determine
    the data memory allocation.  However the grant space lives in the data
    memory outside of any allocated memory section.  As such add a config
    to reserve space for it per app, with a default of 1Kb.
    
    * firmware-bundler: Align data alloc to 4KB (#705)
    
    The data section must begin at a 4Kb offset.
    
    * firmware-bundler: Integrate bundler into builder (#705)
    
    Alter the builder logic to utilize the firmware bundler for creating
    packages rather than containing its own implementation.  This will
    builds to mimic what will be done by 3rd party integrators to provide us
    the ability to detect bugs early and evaluate the usability of the
    application.
    
    * firmware-bundler: Disable flakey test (#705)
    
    Disable `test_firmware_update_flash_successful_fast` as it is currently
    flakey.  Investigation occuring under issue #873.
    
    * firmware-bundler: rom unique name (#705)
    
    Alter the builder process to uniquely name each rom.  This allows
    multiple roms to exist in a directory (like an `all-build`) without
    colliding.

--- tests/integration/src/lib.rs
+++ tests/integration/src/lib.rs
@@ -32,9 +32,7 @@ mod test {
     use caliptra_image_types::FwVerificationPqcKeyType;
     use mcu_builder::flash_image::build_flash_image_bytes;
     use mcu_builder::{CaliptraBuilder, EmulatorBinaries, FirmwareBinaries, ImageCfg, TARGET};
-    use mcu_config::McuMemoryMap;
     use mcu_hw_model::{DefaultHwModel, Fuses, InitParams, McuHwModel};
-    use mcu_image_header::McuImageHeader;
     use mcu_testing_common::{DeviceLifecycle, MCU_RUNNING};
     use random_port::PortPicker;
     use std::sync::atomic::{AtomicU32, Ordering};
@@ -44,7 +42,6 @@ mod test {
         process::Command,
         sync::LazyLock,
     };
-    use zerocopy::IntoBytes;
 
     const TEST_HW_REVISION: &str = "2.1.0";
 
@@ -112,23 +109,15 @@ mod test {
         }
     }
 
-    fn memory_map() -> &'static McuMemoryMap {
-        if cfg!(feature = "fpga_realtime") {
-            &mcu_config_fpga::FPGA_MEMORY_MAP
-        } else {
-            &mcu_config_emulator::EMULATOR_MEMORY_MAP
-        }
-    }
-
     fn compile_rom(feature: &str) -> PathBuf {
         let feature = if TEST_HW_REVISION == "2.1.0" && feature.is_empty() {
             "hw-2-1"
         } else {
             feature
         };
-        let output: PathBuf = mcu_builder::rom_build(Some(platform()), feature)
-            .expect("ROM build failed")
-            .into();
+        let output: PathBuf =
+            mcu_builder::rom_build(Some(platform().to_string()), Some(feature.to_string()))
+                .expect("ROM build failed");
         assert!(output.exists());
         output
     }
@@ -141,22 +130,22 @@ mod test {
         if TEST_HW_REVISION == "2.1.0" {
             features.push("hw-2-1");
         }
-        let feature = feature.map(|f| format!("-{f}")).unwrap_or_default();
-        let output = target_binary(&format!("runtime{}-{}.bin", feature, platform()));
-        let output_name = format!("{}", output.display());
-        mcu_builder::runtime_build_with_apps(
+
+        let platform = platform();
+        let feature_name = match feature {
+            Some(f) => format!("-{f}"),
+            None => String::new(),
+        };
+        let name = format!("runtime{}-{}.bin", feature_name, platform);
+
+        let output = mcu_builder::runtime_build_with_apps(
             &features,
-            Some(output_name),
+            Some(name),
             example_app,
-            Some(platform()),
-            Some(memory_map()),
-            false,
-            None,
-            None,
-            Some(&mcu_config_emulator::flash::LOGGING_FLASH_CONFIG),
+            Some(platform),
             None,
         )
-        .expect("Runtime build failed");
+        .expect("Runtime failed to compile");
         assert!(output.exists());
         output
     }
@@ -857,30 +846,12 @@ mod test {
         } else {
             "test-mcu-svn-lt-fuse"
         };
+        let name = format!("runtime-{}.bin", feature);
+        let test_runtime = target_binary(&name);
 
         println!("Compiling test firmware {}", &feature);
-
-        let test_runtime = target_binary(&format!("runtime-{}.bin", feature));
-        let output_name = format!("{}", test_runtime.display());
-        mcu_builder::runtime_build_with_apps(
-            &[feature],
-            Some(output_name),
-            true,
-            None,
-            None,
-            false,
-            None,
-            None,
-            None,
-            Some(
-                McuImageHeader {
-                    svn: image_svn,
-                    ..Default::default()
-                }
-                .as_bytes(),
-            ),
-        )
-        .expect("Runtime build failed");
+        mcu_builder::runtime_build_with_apps(&[feature], Some(name), true, None, Some(image_svn))
+            .expect("Runtime build failed");
         assert!(test_runtime.exists());
 
         let fuse_vendor_hashes_prod_partition = {
