4a08d8ef85d1ac796e3c88ef318b875d8b0c73e1
Amit Kumar <51718056+amit-kumarh@users.noreply.github.com>
2026-02-27 11:12:42 -0800

    Execute a few test instructions from SRAM after debug unlock (#921)
    * Run RV32 instructions out of SRAM
    
    * Enable flow in prod_debug_unlock test
    
    Signed-off-by: Amit Kumar-Hermosillo <amitkh@google.com>

--- tests/integration/src/jtag/mod.rs
+++ tests/integration/src/jtag/mod.rs
@@ -8,8 +8,7 @@ mod test_uds;
 
 #[cfg(test)]
 mod test {
-
-    use caliptra_hw_model::jtag::DmReg;
+    use caliptra_hw_model::jtag::{CsrReg, DmReg};
     use caliptra_hw_model::openocd::openocd_jtag_tap::OpenOcdJtagTap;
     use caliptra_hw_model::Fuses;
     use mcu_builder::FirmwareBinaries;
@@ -17,7 +16,12 @@ mod test {
     use mcu_hw_model::{DefaultHwModel, InitParams, McuHwModel};
     use romtime::LifecycleControllerState;
 
-    use anyhow::Result;
+    use anyhow::{bail, Result};
+
+    use std::time::Duration;
+
+    pub const ALLHALTED_MASK: u32 = 1 << 9;
+    const DCSR_SET_EBREAKM: u32 = 0x8003; // to enable debug mode on an ebreak
 
     pub fn ss_setup(
         initial_lc_state: Option<LifecycleControllerState>,
@@ -46,18 +50,20 @@ mod test {
     }
 
     /// Write/Read words to SRAM over the system bus.
-    fn sysbus_write_read(tap: &mut OpenOcdJtagTap, sram_base_addr: u32) -> Result<bool> {
-        const NUM_WORDS: u32 = 4;
-        for i in 0..NUM_WORDS {
-            let addr = sram_base_addr + i * 4;
-            let value = 0xa5a5a5a5;
-            tap.write_memory_32(addr, value)?;
+    pub fn sysbus_write_read(
+        tap: &mut OpenOcdJtagTap,
+        sram_base_addr: u32,
+        data: &[u32],
+    ) -> Result<bool> {
+        for i in 0..data.len() {
+            let addr = sram_base_addr + u32::try_from(i)? * 4;
+            tap.write_memory_32(addr, data[i])?;
             let read_value = tap.read_memory_32(addr)?;
             println!(
                 "Wrote 0x{:x} to 0x{:x}; Read 0x{:x}",
-                value, addr, read_value
+                data[i], addr, read_value
             );
-            if value != read_value {
+            if data[i] != read_value {
                 return Ok(false);
             }
         }
@@ -89,10 +95,50 @@ mod test {
         }
 
         // Test writes to Caliptra MCU SRAM.
-        if !sysbus_write_read(mcu_tap, FPGA_MEMORY_MAP.sram_offset)? {
+        if !sysbus_write_read(mcu_tap, FPGA_MEMORY_MAP.sram_offset, &[0xa5a5a5a5])? {
             return Ok(false);
         }
 
         Ok(true)
+    }
+
+    /// Write to and execute from SRAM a tiny assembly program that performs a sum
+    /// and writes it memory, then verify it executed properly
+    pub fn verify_execute_from_sram(tap: &mut OpenOcdJtagTap) -> Result<()> {
+        const SW_OFFSET: u32 = 0x100;
+        const SW_ADDRESS: u32 = FPGA_MEMORY_MAP.sram_offset | SW_OFFSET;
+        const RV32_INSTS: &[u32] = &[
+            0x00100513,                                                           // addi x10, x0, 1
+            0x00200593,                                                           // addi x11, x0, 2
+            0x00b50533,                                              // add x10, x10, x11
+            0x00000637 | (FPGA_MEMORY_MAP.sram_offset & 0xFFFFF000), // lui x12 0xa8c00 (sram base addr)
+            0x00a62023 | (SW_OFFSET & 0x1F) << 7 | (SW_OFFSET >> 5 & 0x7F) << 25, // sw x10, 0x100(x12)
+            0x00100073,                                                           // ebreak
+        ];
+
+        tap.halt()?;
+
+        // Write program to SRAM
+        if !sysbus_write_read(&mut *tap, FPGA_MEMORY_MAP.sram_offset, RV32_INSTS)? {
+            bail!("Readback incorrect on writing program to SRAM")
+        }
+
+        // set PC to start of SRAM
+        tap.write_csr_reg(CsrReg::Dpc, FPGA_MEMORY_MAP.sram_offset)?;
+
+        // Write DCSR to enable ebreakm (bit 15) so ebreak enters debug mode
+        tap.write_csr_reg(CsrReg::Dcsr, DCSR_SET_EBREAKM)?;
+
+        tap.resume()?;
+        tap.wait_status(ALLHALTED_MASK, Duration::from_secs(3))?;
+
+        let readback = tap.read_memory_32(SW_ADDRESS)?;
+        if readback != 3 {
+            bail!(
+                "Readback from MCU SRAM incorrect; got 0x{:x}, wanted 0x3",
+                readback
+            )
+        }
+        Ok(())
     }
 }
