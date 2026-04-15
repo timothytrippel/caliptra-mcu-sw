d69e9076c3cb4c500d323c37f1e52d396fb1288b
Timothy Trippel <5633066+timothytrippel@users.noreply.github.com>
2026-04-15 15:18:05 -0700

    [tests] fix test_bare_metal_runtime_boot test in CI (#1253)
    This change fixes test failures in CI caused by attempting to build
    firmware from source when the source code is unavailable. In CI, the
    test and firmware binaries are built in one CI stage and passed to the
    next stage to run. The following fixes were applied:
    
    1. Update all_build to include the bare-metal runtime in the firmware
       bundle (for emulator platform).
    2. Update test_bare_metal_runtime_boot to use the pre-built bare-metal
       runtime from the bundle.
    
    Signed-off-by: Tim Trippel <ttrippel@google.com>

--- xtask/src/fpga/mod.rs
+++ xtask/src/fpga/mod.rs
@@ -356,16 +356,11 @@ pub(crate) fn fpga_run(args: crate::Commands) -> Result<()> {
         FirmwareBinaries {
             mcu_rom,
             mcu_runtime: blank.to_vec(),
+            mcu_bare_metal_runtime: blank.to_vec(),
             caliptra_rom,
             caliptra_fw: blank.to_vec(),
             soc_manifest: blank.to_vec(),
-            test_roms: vec![],
-            caliptra_test_roms: vec![],
-            test_runtimes: vec![],
-            test_soc_manifests: vec![],
-            test_pldm_fw_pkgs: vec![],
-            test_flash_images: vec![],
-            test_update_flash_images: vec![],
+            ..Default::default()
         }
     };
     let otp_memory = if otp_file.is_some() && otp_file.unwrap().exists() {
